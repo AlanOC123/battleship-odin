cdee38d635ca11ab9a4c75d81bbda2e481dcb5ce
Alan O'Connor <aocthatsme246@gmail.com>
2025-04-07 20:19:50 +0100

    Refined getters and setters for node

--- src/factories/boardNode.js
+++ src/factories/boardNode.js
@@ -2,26 +2,34 @@ const boardNode = (x, y) => {
     const coordinates = [x, y];
     let containsShipPart = false;
     let isHit = false;
+    let element = null;
 
-
-    const render = () => {
+    const renderNode = () => {
         const el = document.createElement('div');
         el.className = "w-full h-full bg-blue-400 hover:bg-blue-500 border border-blue-800 rounded-sm transition transform hover:-translate-y-0.5";
-        return el;
+        element = el;
     };
 
     const hasShip = () => containsShipPart;
+    const setNodeContainsShipPart = (value = true) => (containsShipPart = value);
 
-    const setNodeContainsShipPart = () => containsShipPart = !containsShipPart;
+    const recieveAttack = () => {
+        isHit = true;
+        if (element) {
+            element.className = containsShipPart ? "bg-red-500" : "bg-gray-400"
+        };
 
-    const updateState = () => {
-
-    };
+        return containsShipPart ? "hit" : "miss";
+    }
 
     return {
         hasShip,
         setNodeContainsShipPart,
-        render
+        recieveAttack,
+        renderNode,
+        getCoordinates: () => coordinates,
+        getElement: () => element,
+        getHitState: () => isHit,
     }
 };
 
