53eaa496dd5d88829bef984df980a37968642d5c
Alan O'Connor <aocthatsme246@gmail.com>
2025-04-10 20:11:58 +0100

    Added tests for and tested hashCell and boardNode

--- tests/unit/hashCell.test.js
+++ tests/unit/hashCell.test.js
@@ -1,13 +1,19 @@
 import hashCell from "../../src/helpers/hashCell";
 
-it('Hashes coordinates to keys correctly', () => {
-    expect(hashCell(1, 2)).toBe(`${1}, ${2}`);
-    expect(hashCell(10, 10)).not.ToBe(10, 10);
-});
+describe('hashCell', () => {
+    it('Hashes coordinates to keys correctly', () => {
+        expect(hashCell(1, 2)).toBe(`${1}, ${2}`);
+        expect(hashCell(10, 10)).not.toBe(10, 10);
+    });
 
-it('Throws error with invalid coordinates', () => {
-    expect(() => hashCell(undefined, undefined)).toThrow();
-    expect(() => hashCell(null, null)).toThrow();
-    expect(() => hashCell(undefined, 1)).toThrow();
-    expect(() => hashCell(null, 1)).toThrow();
+    it('Throws error with invalid coordinates', () => {
+        expect(() => hashCell(undefined, undefined)).toThrow();
+        expect(() => hashCell(null, null)).toThrow();
+        expect(() => hashCell(undefined, 1)).toThrow();
+        expect(() => hashCell(null, 1)).toThrow();
+    });
+
+    it ('Treats x, y and y, x to be distinct', () => {
+        expect(hashCell(3, 4)).not.toBe(`${4}, ${3}`);
+    });
 });
